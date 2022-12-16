2e690a8d7bb9539c3a40dd5d6cfcac20b6572d4a
Dorothy <dorothychauu@gmail.com>
2022-12-16 16:39:44 -0500

    commit tree b63af8195547466e356a049c4111757f67601dad

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+      })
+    .then(resp => resp.json())
+    .then(object => document.body.append(object))
+    .catch(error => document.body.append(error.message))
+  }
+
+// function submitData(name,email) {
+//     fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//         },
+//         body: JSON.stringify({
+//             name: name,
+//             email:email,
+//         }),
+//     })
+//     .then(resp => resp.json())
+//     .then(object => body.append(object))
+//     .catch(error => body.append(error.message))
+// }
